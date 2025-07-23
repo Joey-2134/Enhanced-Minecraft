449b0e1389b4814aa5b24a138a5b7697388d1ceb
joeyg <joeygalvin2134@gmail.com>
2025-07-24 00:48:12 +0100

    Added fertilizer NOOP

--- Enhanced-Farming/src/main/java/com/enhancedfarming/EnhancedFarming.java
+++ Enhanced-Farming/src/main/java/com/enhancedfarming/EnhancedFarming.java
@@ -1,5 +1,6 @@
 package com.enhancedfarming;
 
+import com.enhancedfarming.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -7,18 +8,11 @@ import org.slf4j.LoggerFactory;
 
 public class EnhancedFarming implements ModInitializer {
 	public static final String MOD_ID = "enhanced-farming";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing {}", MOD_ID);
+		ModItems.initialize();
 	}
 }
