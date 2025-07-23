2b8e7e589fba06df0b20bbaca29c13a64bde7a6b
joeyg <joeygalvin2134@gmail.com>
2025-07-23 23:50:16 +0100

    init commit

---
+++ Enhanced-Farming/src/client/java/com/enhancedfarming/EnhancedFarmingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.enhancedfarming;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnhancedFarmingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
