7809b10334ee090d6a860c61edf8cbc446750c11
Joey Galvin <joeygalvin2134@gmail.com>
2025-07-27 14:25:00 +0100

    added custom item group

--- Enhanced-Farming/src/main/java/com/enhancedfarming/interfaces/ItemSettings.java
+++ Enhanced-Farming/src/main/java/com/enhancedfarming/interfaces/ItemSettings.java
@@ -3,7 +3,7 @@ package com.enhancedfarming.interfaces;
 import net.minecraft.item.Item;
 
 public interface ItemSettings {
-    public static Item.Settings getSettings() {
+    static Item.Settings getSettings() {
         return new Item.Settings();
     }
 }
--- Enhanced-Farming/src/main/java/com/enhancedfarming/items/ModItems.java
+++ Enhanced-Farming/src/main/java/com/enhancedfarming/items/ModItems.java
@@ -1,49 +1,57 @@
 package com.enhancedfarming.items;
 
-import com.enhancedfarming.EnhancedFarming;
-import com.enhancedfarming.interfaces.ItemSettings;
 import com.enhancedfarming.items.settings.FertilizerSettings;
 import com.enhancedfarming.items.settings.RottenAppleSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
-import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
 import java.util.function.Function;
 
+import static com.enhancedfarming.EnhancedFarming.MOD_ID;
+
 public class ModItems {
+
 
     public static final Item FERTILIZER = register("fertilizer", FertilizerItem::new, FertilizerSettings.getSettings());
     public static final Item ROTTEN_APPLE = register("rotten_apple", RottenAppleItem::new, RottenAppleSettings.getSettings());
 
+    public static final RegistryKey<ItemGroup> ENHANCED_FARMING_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MOD_ID, "item_group"));
+    public static final ItemGroup ENHANCED_FARMING_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.FERTILIZER))
+            .displayName(Text.translatable("itemGroup.enhanced-farming-group"))
+            .build();
+
+    private static final List<Item> ENHANCED_FARMING_ITEMS = List.of(
+            FERTILIZER,
+            ROTTEN_APPLE
+    );
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
-        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(EnhancedFarming.MOD_ID, name));
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
         Item item = itemFactory.apply(settings.registryKey(itemKey));
 
         Registry.register(Registries.ITEM, itemKey, item);
-
         return item;
     }
 
     public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                .register((itemGroup) -> itemGroup.add(FERTILIZER));
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
-                        .register((itemGroup) -> itemGroup.add(ROTTEN_APPLE));
-
-        FuelRegistryEvents.BUILD.register((builder, context) -> {
-//            builder.add(FERTILIZER, 30 * 20); // EXAMPLE
-        });
-
+        addModItemsToCustomModGroup();
         CompostingChanceRegistry.INSTANCE.add(ROTTEN_APPLE, 0.3f);
+    }
+
+    private static void addModItemsToCustomModGroup() {
+        Registry.register(Registries.ITEM_GROUP, ENHANCED_FARMING_GROUP_KEY, ENHANCED_FARMING_GROUP);
+        ItemGroupEvents.modifyEntriesEvent(ENHANCED_FARMING_GROUP_KEY)
+                .register(itemGroup -> ModItems.ENHANCED_FARMING_ITEMS.forEach(itemGroup::add));
     }
 
 }
